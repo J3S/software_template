315b905c720e93bf90ed3320030283250ccb3c0d
J3S <johnnysuarez@outlook.com>
2016-04-25 18:18:56 -0500

    Agregado submenus - redimensionamiento del sidebar si la pantalla es pequeña

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,4 +3,20 @@ $(document).ready(function(){
     e.preventDefault();
     $("#sidebar-container").toggleClass("toggled");
   });
+  $('#sidebar-items ul').hide();
+  $('#sidebar-items ul').children('.current').parent().show();
+  $('#sidebar-items li a').click(function() {
+      var checkElement = $(this).next();
+      if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+          $('#menu ul:visible').slideDown('normal');
+          checkElement.slideUp('normal');
+        return false;
+      }
+      if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+        $('#sidebar-items ul:visible').slideUp('normal');
+          checkElement.slideDown('normal');
+          return false;
+        }
+      }
+    );
 });
